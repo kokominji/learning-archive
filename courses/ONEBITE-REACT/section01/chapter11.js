cd0e392946bb01fd290405f9809e04b60b2067c5
kokominji <kmj6386@gmail.com>
2025-10-25 03:25:51 +0900

    learing: 10/25

---
+++ courses/ONEBITE-REACT/section01/chapter11.js
@@ -0,0 +1,17 @@
+// 함수선언
+function getArea(width, height) {
+  function another() { //중첩 함수
+    console.log("another");
+  }
+  another();
+  let area = width * height;
+
+  return area;
+}
+let area1 = getArea(10, 20);
+console.log(area1);
+
+let area2 = getArea(30, 20);
+console.log(area2);
+
+getArea(120, 200);
