83af8bc7dd673f4b9700117478cccf767dc6f5c4
kokominji <kmj6386@gmail.com>
2025-11-02 00:09:32 +0900

    finish: section03

--- courses/ONEBITE-REACT/section03/src/index.js
+++ courses/ONEBITE-REACT/section03/src/index.js
@@ -1,4 +1,11 @@
-const { add, sub } = require("./math");
+import mul from "./math.js";
+import { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(3, 2));
