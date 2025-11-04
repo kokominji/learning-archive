2f7590cc896dd0cff70e5e8397646cb683529e16
kokominji <kmj6386@gmail.com>
2025-11-04 23:48:00 +0900

    learing: 11/4

---
+++ courses/ONEBITE-REACT/section05/src/components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = (props) => {
+  console.log(props);
+  return (
+    <button style={{ color: props.color }}>
+      {props.text} - {props.color.toUpperCase()}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "black",
+};
+
+export default Button;
