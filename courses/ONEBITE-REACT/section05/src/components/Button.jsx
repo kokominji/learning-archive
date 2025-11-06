8e62f2076a3001040624da1cd3b289ef86233cd3
kokominji <kmj6386@gmail.com>
2025-11-07 01:39:35 +0900

    learing: 11/7

--- courses/ONEBITE-REACT/section05/src/components/Button.jsx
+++ courses/ONEBITE-REACT/section05/src/components/Button.jsx
@@ -1,12 +1,19 @@
-const Button = ({children, text, color = "black"}) => {
+const Button = ({ children, text, color = "black" }) => {
+  //이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
   );
 };
-
-
 
 export default Button;
