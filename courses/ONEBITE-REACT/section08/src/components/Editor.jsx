21532a23d792b4010ffacffa4f87973f9911279a
kokominji <kmj6386@gmail.com>
2025-11-09 23:52:19 +0900

    feat: todo추가:

--- courses/ONEBITE-REACT/section08/src/components/Editor.jsx
+++ courses/ONEBITE-REACT/section08/src/components/Editor.jsx
@@ -1,10 +1,37 @@
 import "./Editor.css";
+import { useState, useRef } from "react";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContnet] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContnet(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onsubmit();
+    }
+  };
+  const onsubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContnet("");
+  };
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onsubmit}>추가</button>
     </div>
   );
 };
