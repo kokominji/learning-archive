b8ad73a46fdea1abbacb267120a9b11995ae9901
kokominji <kmj6386@gmail.com>
2025-11-09 22:18:28 +0900

    section07: deps

---
+++ courses/ONEBITE-REACT/section07/src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  // 1. 마운트 : 탄생
+  // 2. 업데이트 : 변화, 리렌더링
+  // 3. 언마운트 : 죽음
+
+  useEffect(() => {
+    console.log(`count: ${count} / input:${input}`);
+  }, [count, input]);
+  // 의존성 배열
+  // dependency array
+  // deps
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <Viewer count={count} />
+      </section>
+
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
