b0c4f8b60667a186c30c22ecff8d260d093ac505
고민지 <kmj6386@gmail.com>
2025-12-26 18:02:50 +0900

    study

--- courses/ONEBITE-REACT/section10/src/components/List.jsx
+++ courses/ONEBITE-REACT/section10/src/components/List.jsx
@@ -19,9 +19,30 @@ const List = ({ todos, onUpdate, onDelete }) => {
   };
 
   const filteredTodos = getFilteredData();
+
+  const getAnalyzedData = () => {
+    console.log("getAnalyzed 호출!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  };
+
+  const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
   return (
     <div className="List">
       <h4>Todo List ❤️</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone:{notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
