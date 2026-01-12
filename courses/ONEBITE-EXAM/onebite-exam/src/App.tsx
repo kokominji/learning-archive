df2636fa7f1fd78adb50e0db0a5adb8483698875
kokominji <kmj6386@gmail.com>
2026-01-12 22:47:33 +0900

    실습환경 구축

---
+++ courses/ONEBITE-EXAM/onebite-exam/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TodoListPage from "./pages/todo-list-page";
+
+function App() {
+  return (
+    <div>
+      <TodoListPage />
+    </div>
+  );
+}
+
+export default App;
