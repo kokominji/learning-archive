82e8f04cbbf70a515efd7c743336c00dedbf13c7
고민지 <kmj6386@gmail.com>
2026-02-04 17:58:29 +0900

    2/4

--- courses/ONEBITE-EXAM/onebite-exam/src/pages/test.tsx
+++ courses/ONEBITE-EXAM/onebite-exam/src/pages/test.tsx
@@ -1,15 +1,20 @@
 import { CheckTree } from "rsuite";
-// import { MockTreeData } from "./mock";
 import { useEffect, useRef, useState } from "react";
 import "rsuite/CheckTree/styles/index.css";
+import {
+  MdOutlineKeyboardArrowDown,
+  MdOutlineKeyboardArrowRight,
+  MdAttachFile,
+  MdOutlineFolderOpen,
+} from "react-icons/md";
 
 export const Example = () => {
   const [checkedValuesState, setCheckedValuesState] = useState<
     (string | number)[]
   >([]); //체크박스
   const [treeDataState, setTreeDataState] = useState<any[]>([]);
-
   const treeDataRef = useRef(null);
+  const checkedNodeRef = useRef<any[]>([]);
 
   useEffect(() => {
     const fetchTreeData = async () => {
@@ -29,44 +34,79 @@ export const Example = () => {
       }
       if (node.children) {
         const childrenNode = findNodeByValue(node.children, Value);
+        // console.log("childrenNode", childrenNode);
         if (childrenNode) return childrenNode;
       }
     }
     return null;
   };
+  // console.log("treeDataState", treeDataState);
+  // console.log("checkedValuesState", checkedValuesState);
 
   const onClick = (e: any) => {
+    // 코드 수정필요
+    const nodeValue = e.target
+      .closest(".rs-check-tree-node")
+      ?.querySelector("input")?.value;
+    const nodeData = findNodeByValue(treeDataState, nodeValue);
+
     if (e.target.className === "rs-checkbox-label") {
       e.preventDefault();
-      console.log("e", e);
-
-      const nodeValue = e.target
-        .closest(".rs-check-tree-node")
-        ?.querySelector("input")?.value;
-
       console.log("nodeValue", nodeValue);
 
       if (nodeValue) {
-        const nodeData = findNodeByValue(treeDataState, nodeValue);
-        // console.log("전체 노드 데이터:", nodeData);
-        // const dataId = e.target.ownerDocument.activeElement.value;
-        // console.log("dataId", dataId);
-
-        // treeDataRef.current = findNodeById(treeData, dataId);
-        // console.log("treeDataRef.current", treeDataRef.current);
+        console.log("nodeData", nodeData);
       }
+    } else if (e.target.type === "checkbox") {
+      e.preventDefault();
+
+      console.log("nodeData", nodeData);
+
+      const existingIndex = checkedNodeRef.current.findIndex(
+        (node) => node.refKey === nodeData?.refKey,
+      );
+
+      if (existingIndex === -1) {
+        checkedNodeRef.current = [...checkedNodeRef.current, nodeData];
+      } else {
+        checkedNodeRef.current = checkedNodeRef.current.filter(
+          (item, index) => index !== existingIndex,
+        );
+      }
+
+      console.log("checkedNodeRef", checkedNodeRef.current);
     }
   };
 
+  // 아이콘
+  const TreeNode = ({
+    children,
+    ...rest
+  }: {
+    children: React.ReactNode;
+    [key: string]: any;
+  }) => {
+    return (
+      <div {...rest} style={{ display: "flex", alignItems: "center", gap: 4 }}>
+        {children}
+      </div>
+    );
+  };
+
   return (
-    <CheckTree
-      ref={treeDataRef}
-      data={treeDataState}
-      value={checkedValuesState}
-      defaultExpandAll
-      onClick={onClick}
-      onChange={setCheckedValuesState}
-      height={500}
-    />
+    <>
+      {treeDataState.length > 0 && (
+        <CheckTree
+          ref={treeDataRef}
+          data={treeDataState}
+          value={checkedValuesState}
+          defaultExpandAll
+          onClick={onClick}
+          onChange={setCheckedValuesState}
+          height={500}
+          showIndentLine
+        />
+      )}
+    </>
   );
 };
